e2525571eadf0cac0a4f2abded58ae55118e6268
kiani <codewithkiani@gmail.com>
2025-02-06 23:33:46 +0330

    Add the slug trait

---
+++ src/Domain/Shared/Models/Concerns/HasSlug.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Domain\Shared\Models\Concerns;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+trait HasSlug
+{
+    public static function bootHasSlug(): void
+    {
+        static::creating(
+            fn(Model $model) => $model->slug = Str::slug($model->title),
+        );
+    }
+}
