f2f9407c45ee17b0e3b2d960e0b249dcd5e262b0
kiani <codewithkiani@gmail.com>
2025-02-07 12:48:52 +0330

    Add the user and threads endpoints

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -1,1 +1,41 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+
+/**
+ * User Endpoints
+ */
+Route::group([
+    'prefix' => 'users',
+    'as' => 'users:',
+    'middleware' => 'auth:sanctum',
+], function (){
+    Route::get(
+        '/{user}',
+        action: \App\Http\Controllers\Api\V1\User\ShowController::class,
+    )->name('show');
+});
+
+/**
+ * Thread Endpoints
+ */
+Route::group([
+    'prefix' => 'threads',
+    'as' => 'threads:',
+    'middleware' => 'auth:sanctum'
+], function () {
+    Route::get(
+        '',
+        action: \App\Http\Controllers\Api\V1\Threads\IndexController::class
+    )->name('index');
+
+    Route::get(
+        '/{thread}',
+        action: \App\Http\Controllers\Api\V1\Threads\ShowController::class
+    )->name('show');
+
+    Route::post(
+        '',
+        action: \App\Http\Controllers\Api\V1\Threads\StoreController::class
+    )->name('store');
+});
