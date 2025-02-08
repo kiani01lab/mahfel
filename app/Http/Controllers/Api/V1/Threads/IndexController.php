e1ad2437114201973b9ffa1151e1b0ca7679a6a9
kiani <codewithkiani@gmail.com>
2025-02-08 08:11:04 +0330

    Add the index and show controller for threads

---
+++ app/Http/Controllers/Api/V1/Threads/IndexController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Threads;
+
+use App\Http\Controllers\Api\V1\ApiController;
+use App\Http\Resources\Api\V1\Thread\ThreadResource;
+use Domain\Threads\Models\Thread;
+use Illuminate\Http\Request;
+
+class IndexController extends ApiController
+{
+    public function __invoke(Request $request)
+    {
+        if($this->includedFormQueryParams('author')){
+            return response()->json(
+                data: ThreadResource::collection(
+                    resource: Thread::with('author')->published()->paginate(10)
+                ),status: 200
+            );
+        }
+
+        return response()->json(
+            data: ThreadResource::collection(
+                resource: Thread::published()->paginate(10)
+            ),status: 200
+        );
+    }
+}
---
+++ app/Http/Controllers/Api/V1/Threads/ShowController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Threads;
+
+use App\Http\Controllers\Api\V1\ApiController;
+use App\Http\Resources\Api\V1\Thread\ThreadResource;
+use Domain\Threads\Models\Thread;
+
+class ShowController extends ApiController
+{
+    public function __invoke(Thread $thread)
+    {
+        if($this->includedFormQueryParams('author')){
+            return response()->json(
+                data: new ThreadResource($thread->load('author')),
+                status: 200,
+            );
+        }
+
+        return response()->json(
+            data: new ThreadResource($thread),
+            status: 200,
+        );
+    }
+}
